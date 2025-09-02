d3ccb7273c59c94c3fc12e1bc1cc9804daa80536
manel <manelbng@gmail.com>
2025-09-02 15:22:37 +0200

    update preprocesing files

--- example_simple_pandas/preprocess.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-df['sepal_area'] = df['sepal_length'] * df['sepal_width']
-df.to_csv('/output/processed.csv', index=False)
